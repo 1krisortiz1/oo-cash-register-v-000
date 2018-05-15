71f048efe5d0777e316a7e11f75fd8f5cb5f28a1
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-15 18:32:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     self.total += price * quantity
   end
 
-  def apply_discount(total, discount, items)
+  def apply_discount(total, discount)
     self.total * discount
   end
 
