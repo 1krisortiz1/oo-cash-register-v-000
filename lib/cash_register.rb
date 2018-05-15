fa2ab9d28ebd27d3e8702e351d8b6b7a14379853
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-15 20:01:00 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,8 +11,6 @@ class CashRegister
     self.total += price * quantity
     quantity.times { items << title }
     self.last = price * quantity
-
-
   end
 
   def apply_discount
@@ -25,7 +23,7 @@ class CashRegister
   end
 
   def items
-    self.items = []
+    .items
   end
 
   def void_last_transaction
